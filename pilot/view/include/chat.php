be3d295df6f1fb75fe4cc736a40db8d4956cf548
Kathy Flint <kathy.flint@northbridgetech.org>
2015-04-17 17:08:15 -0500

    nav and style updates

--- pilot/view/include/chat.php
+++ pilot/view/include/chat.php
@@ -14,7 +14,7 @@ while ($row = pg_fetch_array($cursor)) {
 ?>
 
 <div class="topLeftQuad">
- 	<a href="#">Community Conference</a>
+ 	<b>Web Conference Room</b>
  	<p style="margin-left:20px;">To start or join a meeting, simply click the button!</p>
  	<div style="margin-left:20px;margin-top:10px;">
  	<!--
@@ -29,10 +29,10 @@ while ($row = pg_fetch_array($cursor)) {
 	<!-- TODO - something comparable with new system -->
 		<? if (isset($roomLink) && strlen($roomLink) > 1) {
 				$formattedLink = "http://conference.northbridgetech.org" . $roomLink;
-				$displayText = "Your Virtual Room Pass";
+				$displayText = "Your Conference Room Pass";
 			} else {
 				$formattedLink = "#";
-				$displayText = "Your Virtual Room Pass";;
+				$displayText = "Your Conference Room Pass";;
 			}
 		?>
 		<p>
@@ -43,23 +43,23 @@ while ($row = pg_fetch_array($cursor)) {
  	</a></p>
 	</div>
   &nbsp;<br/>
-	<a href="#">Tutorial</a>
+	<b>Tutorial</b>
 	<p style="margin-left:20px;">How to join your computer to a web conference</p>
 	<div style="margin-left:20px;margin-top:10px;">
 		<iframe width="280" height="157" src="https://www.youtube.com/embed/4Y__UsUrRx0" frameborder="0" allowfullscreen></iframe>
 	</div>
 	&nbsp;<br/>
-	<a href="#">Training</a>
+	<b>Training</b>
 </div>
 <div class="lowerTraining">
 
 <p style="margin-left:20px;"><b>Ready to give web conferencing a try?</b></p>
 <p style="margin-left:20px;">All <? echo $_SESSION['networkName']; ?> pilot users are eligible for free, personalized, no-obligation web conference training. This will help your network to take full advantage of the benefits of web conferencing.</p>
-<p style="margin-left:20px;"><a href="">Register for a free, no-obligation training session</a></p>
+<p style="margin-left:20px;"><a href="http://www.eventbrite.com/e/nexus-web-conference-training-tickets-16633180290" target="_blank">Register for a free, no-obligation training session</a></p>
 </div>
  	
 <div class="rightColumnPlain" style="left:360px;width:450px;">	
-	 	<a href="#">Your System Tech Check</a>
+	 	<b>Your System Tech Check</b>
 	 <iframe src="http://northbridgetech.org/<? echo $env_appRoot; ?>/nexus/view/include/conferenceTechCheck.html" width="100%" height="75%" frameborder="0" scrolling="no"></iframe> 
 </div>
 
