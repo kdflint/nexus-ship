187638b85f69a8337fbe5dc4442cc8735ea27a44
Kathy Flint <kathy.flint@northbridgetech.org>
2015-09-11 11:09:46 -0500

    #187

--- nexus/web/src/user/User.php
+++ nexus/web/src/user/User.php
@@ -44,7 +44,7 @@ class User {
 		// TODO: fix up network id determination (parent, god, etc)
 		// TODO - this will fail if user in > 1 group
 		$query = "
-			select u.id as id, u.fname as fname, u.lname as lname, u.password as password, u.conference_link as link, u.email as email, ug.role_fk as roleid, o1.name as affiliation, o1.id as affiliationid, o1.uid as affiliationuid, o2.name as network, o2.id as networkid, o2.logo as logo, uo.role_fk as role
+			select u.id as id, u.fname as fname, u.lname as lname, u.password as password, u.conference_link as link, u.email as email, ug.role_fk as roleid, o1.name as affiliation, o1.id as affiliationid, o1.uid as affiliationuid, o2.name as network, o2.id as networkid, o1.logo as logo, uo.role_fk as role
 			from public.user u, user_organization uo, user_group ug, organization o1, organization o2, organization_organization oo
 			where u.username = $1
 			and uo.user_fk = u.id
