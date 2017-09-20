598b6916b89c3cb8c2056e95c7db15cd69f76c86
Hema-Nambi <hemalatha.nambiradje@gmail.com>
2017-09-19 20:20:49 -0400

    Add files via upload

---
+++ nexus/tests/selenium_wd/Config_LI.java
@@ -0,0 +1,64 @@
+package LinkedIn_Login;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Config_LI
+{
+	public static WebDriver driver;
+ 	public static StringBuffer verificationErrors = new StringBuffer();
+ 	
+ 	
+ 	//Path of property file in local - Need to be replaced 
+ 	static File property_file = new File("C:\\Users\\Arvind\\Desktop\\hema practie selenium\\Story 2 - LI Login\\datafile_LI.properties");
+  
+ 	static FileInputStream fileInput = null;
+ 	static Properties prop = new Properties();
+ 	
+ 	@BeforeSuite(alwaysRun = true)
+	  public void setUp() throws Exception
+	  {
+	      try
+	      {
+	    	  fileInput = new FileInputStream(property_file);
+		  }
+	      catch(FileNotFoundException e)
+	      {
+		  		e.printStackTrace();
+		  }
+		  try
+		  {
+			  prop.load(fileInput);
+		  }
+		  catch(IOException e)
+		  {
+			  e.printStackTrace();
+		  }
+		  
+		  ChromeOptions options = new ChromeOptions();
+		  options.addArguments("--disable-notifications");
+		  
+		  
+		  System.setProperty("webdriver.chrome.driver",prop.getProperty("chromedriver"));
+		  driver=new ChromeDriver(options);
+		  
+		 
+	  }
+ 	
+ 	
+ 	@AfterSuite
+ 	public void closeBrowser() 
+ 	{
+ 		driver.quit();
+ 	}
+
+}
