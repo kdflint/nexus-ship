3a4a4e4c18237d4c64b9b77b89dfac0dec2640b5
NorthBridge Technology Alliance <kathy.flint@northbridgetech.org>
2018-04-14 17:50:45 -0500

    Create db_create.sql

---
+++ setup/db_create.sql
@@ -0,0 +1,11 @@
+-- Database: localnexus
+
+-- DROP DATABASE localnexus;
+
+CREATE DATABASE localnexus
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
