3e616064bcfc260548eb213892fcf5508f8e9c58
Kathy Flint <kathy.flint@northbridgetech.org>
2018-04-17 17:11:21 -0500

    update db setup file with data inserts

--- setup/db_create.sql
+++ setup/db_create.sql
@@ -4,8 +4,24 @@
 
 CREATE DATABASE localnexus
   WITH OWNER = postgres
-       ENCODING = 'UTF8'
+			 ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
+			 LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
        CONNECTION LIMIT = -1;
+       
+-- Role: northbri_devnexus
+
+-- DROP ROLE northbri_devnexus;
+
+CREATE ROLE northbri_devnexus LOGIN
+  ENCRYPTED PASSWORD 'password'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+  
+ -- Role: northbri_web
+
+-- DROP ROLE northbri_web;
+
+CREATE ROLE northbri_web LOGIN
+  ENCRYPTED PASSWORD 'password'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
