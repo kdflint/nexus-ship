c48d4e37ab796d018f5c8fbd03feef98d48556a4
kdflint <kathy.flint@northbridgetech.org>
2019-06-27 19:55:52 -0500

    top menu bar

--- nexus/web/src/chromeExtn/background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab){
-    console.log(tab);
-    var action_url = "https://northbridgetech.org/apps/nexus/web/login.php";
-    chrome.tabs.create({url: action_url});
-}
