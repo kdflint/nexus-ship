336a05a19d412ebd92a907f57b8918a31f0dc196
kdflint <kathy.flint@northbridgetech.org>
2020-09-28 10:56:37 -0500

    add dial-in formatting calls to event view

--- nexus/web/scripts/javascriptHandler.php
+++ nexus/web/scripts/javascriptHandler.php
@@ -2551,3 +2551,17 @@ function protocolize(url) {
 		return url;
 	}
 }
+
+function formatConfDialNumber(confNumber) {
+  var cleaned = ('' + confNumber).replace(/\D/g, '');
+  var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    var intlCode = (match[1] ? '+1 ' : '');
+    return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+  }
+  return confNumber;
+}
+
+function formatConfPinNumber(pinNumber) {
+  return pinNumber;
+}
